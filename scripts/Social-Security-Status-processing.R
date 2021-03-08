ebfe910ba1af3e0a29bd283d784fd21e0ef86ea9
ilyankou <ilyankou@gmail.com>
2021-03-08 10:19:40 +0000

    Update ACS 2019 5y

--- scripts/Social-Security-Status-processing.R
+++ scripts/Social-Security-Status-processing.R
@@ -18,7 +18,7 @@ source('./scripts/acsHelpers.R')
 ctGeos <- getCTGeos("town")
 # years for which we will process this dataset
 #yearList = c(2015)
-yearList = c(2010:2018)
+yearList = c(2010:2019)
 
 tn = "B19055"
 acsdata <- getACSData(ctGeos, yearList = yearList, table = tn)
@@ -146,7 +146,7 @@ dataset <- dataset %>%
 # Write to File
 write.table(
     dataset,
-    file.path(getwd(), "data", "social-security-status-2018.csv"),
+    file.path(getwd(), "data", "social-security-status-2019.csv"),
     sep = ",",
     row.names = F,
     na = "-9999"
